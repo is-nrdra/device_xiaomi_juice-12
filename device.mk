d30feff2d14aa7b9fbb87414319053e9af92f573
Muhammad Fadly A S <mhmmdfdlyas@gmail.com>
2021-10-14 15:31:04 +0700

    juice: rootdir: Initial custom init script for juice
    Change-Id: I31df30aba365d9c81dd0c65f7349ab53ba332563

--- device.mk
+++ device.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc \
-    init.target.rc
+    init.target.rc \
+    init.juice.rc
 
 # Input
 PRODUCT_COPY_FILES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,9 +9,17 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.target.rc
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init.target.rc
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.juice.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.juice.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
 include $(BUILD_PREBUILT)
