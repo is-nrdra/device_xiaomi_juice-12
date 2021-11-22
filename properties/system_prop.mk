72de2dec2d13dbc9732854c3e4c5d8bcd640ae4e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-22 14:29:18 +0530

    juice: Disable unspecialized app process pool
     * doesnt work properly with zygote native fork loop:
    
    11-16 13:12:39.971  2350  2350 E USAP    : Attempt to invoke virtual method 'int com.android.internal.os.ZygoteCommandBuffer.getCount()' on a null object reference
    11-16 13:12:39.973  1952  2099 E ZygoteProcess: IO Exception while communicating with USAP pool - Connection reset by peer
    
    Change-Id: I7dc09be8f047c29400c1df7874f5a0e1dae81723

--- properties/system_prop.mk
+++ properties/system_prop.mk
@@ -56,10 +56,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
 
-# Zygote
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.device_config.runtime_native.usap_pool_enabled=true
-
 # Charger
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.charger.enable_suspend=true \
